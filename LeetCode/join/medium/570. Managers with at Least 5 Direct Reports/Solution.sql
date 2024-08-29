1a923c85fe9013ba5efc8c3c780242b9ece8b35b
carlosh-rios <hortarios.carlos@gmail.com>
2024-08-29 14:24:19 +0100

    feat: LeetCode - medium problems

---
+++ LeetCode/join/medium/570. Managers with at Least 5 Direct Reports/Solution.sql
@@ -0,0 +1,15 @@
+Create table If Not Exists Employee (id int, name varchar(255), department varchar(255), managerId int)
+Truncate table Employee
+insert into Employee (id, name, department, managerId) values ('101', 'John', 'A', 'None')
+insert into Employee (id, name, department, managerId) values ('102', 'Dan', 'A', '101')
+insert into Employee (id, name, department, managerId) values ('103', 'James', 'A', '101')
+insert into Employee (id, name, department, managerId) values ('104', 'Amy', 'A', '101')
+insert into Employee (id, name, department, managerId) values ('105', 'Anne', 'A', '101')
+insert into Employee (id, name, department, managerId) values ('106', 'Ron', 'B', '101')
+
+
+
+SELECT e1.name FROM Employee e1 
+INNER JOIN Employee e2 ON e2.id != e1.id AND e2.managerId = e1.id
+GROUP BY (e2.managerId,e1.name)
+HAVING COUNT(*) >= 5
